bc1c41d1bde8e948f56ffee357ef0f2291eba8ad
U-BENS-PC\benhi <benhimes12@gmail.com>
2026-02-09 21:24:16 -0700

    Final submission

---
+++ labs/Math/addtwonumbers/main.cpp
@@ -0,0 +1,23 @@
+/* Lab: Basic Math
+
+
+Updated by : Ben Himes
+Date: 2/9/2026
+CSCI 111
+Instructor: Professor James
+*/
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int a, b, ans;
+   cin >> a;
+   cin >> b;
+   ans = a + b;
+   cout << ans << endl;
+   return 0;
+}
